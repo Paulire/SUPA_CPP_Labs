3a762a110adeb721162db86e3a0dd72f3950ff74
Paul <67006101+Paulire@users.noreply.github.com>
2023-12-06 14:37:42 +0000

    Added

--- Exercises2023/Ex3_4/normFunctions.h
+++ Exercises2023/Ex3_4/normFunctions.h
@@ -1,6 +1,8 @@
 #include "FiniteFunctions.h"
 
 class NormFunction : FiniteFunction {
+public:
+
 private:
     double invxsquared( double x, double mu, double sigma ); //The default functional form
 };
